d446473ea2af1fe330f8b536b7e4cd4082916f6b
Alex <alex.sudom@gmail.com>
2018-03-02 17:38:46 +0200

    fix compilation error: Greeter.java:[19,15] error: cannot find symbol

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,11 @@ public class Greeter {
 
   }
 
-/**
- *@param someone the name of a param
- *@return greeting string
-**/
-  public final tring greet(final String someone) {
+  /**
+  * @param someone the name of a param
+  * @return greeting string
+  */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
